4d3d33d41ee67e82f2a93ae409b3da2705378668
adilalimgozha <alimgozha2003@gmail.com>
2024-10-22 01:14:23 +0500

    registration-login urls, pages, forms

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_list_or_404
+from django.contrib import messages
+from .forms import UserRegistationForm
+from .models import User
 
-# Create your views here.
+
+def registration(request):
+    if request.method == "POST":
+        user_form = UserRegistationForm(request.POST)
+
+        if user_form.is_valid():
+            user = user_form.save()
+
+        redirect("login")
+
+    else:
+        user_form = UserRegistationForm()
+
+    return render(request, 'registration.html', {'user_form': user_form})
+
+
