4d967f91321ebb908bd568967abcc7bd39f5ebc0
neemaque <nurmukhan.n.b@gmail.com>
2024-10-23 02:49:23 +0500

    Create, delete, edit, see reviews

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,10 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("registration/", views.registration, name="registration"),
-    path("login/", auth_views.LoginView.as_view(template_name = 'login.html'), name='login')
+    path("login/", auth_views.LoginView.as_view(template_name = 'login.html'), name='login'),
+    path("hotels/<int:hotel_id>/reviews", views.hotel_reviews, name="hotel_reviews"),
+    path('hotels/<int:hotel_id>/reviews/create/', views.create_review, name='create_review'),
+    path('hotels/<int:hotel_id>/reviews/<int:review_id>/edit', views.edit_review, name='edit_review'),
+    path('hotels/<int:hotel_id>/reviews/<int:review_id>/delete', views.delete_review, name='delete_review'),
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, redirect, get_list_or_404
+from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404
 from django.contrib import messages
 from .forms import UserRegistationForm
 from .models import User, Favorites, Employee, Review
+from hotel.models import Hotel
+from .forms import ReviewForm
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
 def registration(request):
@@ -18,4 +22,54 @@ def registration(request):
 
     return render(request, 'registration.html', {'user_form': user_form})
 
+
+def hotel_reviews(request, hotel_id):
+    hotel = get_object_or_404(Hotel, id=hotel_id)
+    reviews = Review.objects.filter(hotel=hotel)
+    return render(request, 'hotel_reviews.html', {'hotel': hotel, 'reviews': reviews})
+
+
+@login_required(login_url='/login/')
+def create_review(request, hotel_id):
+    hotel = get_object_or_404(Hotel, id=hotel_id)
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.hotel = hotel
+            review.user = request.user
+            review.save()
+            return redirect('hotels')  # Redirect to the hotel reviews page
+    else:
+        form = ReviewForm()
+
+    return render(request, 'create_review.html', {'form': form, 'hotel': hotel})
+
+
+@login_required(login_url='/login/')
+def edit_review(request, hotel_id, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    if review.user != request.user:
+        return HttpResponse('Error')
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            return redirect('hotel_reviews', hotel_id=review.hotel.id)  # Redirect to the hotel reviews page
+    else:
+        form = ReviewForm(instance=review)
+
+    return render(request, 'edit_review.html', {'form': form, 'review': review})
+
+@login_required(login_url='/login/')
+def delete_review(request, hotel_id, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    if review.user != request.user:
+        return HttpResponse('Error')
+    if request.method == 'POST':
+        review.delete()
+        return redirect('hotel_reviews', hotel_id=review.hotel.id)  # Redirect to the hotel reviews page
+
+    return render(request, 'delete_review.html', {'review': review})
 
