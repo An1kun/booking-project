7cbb6726279e0762b217e6f29bb9ed3775655f5f
adilalimgozha <alimgozha2003@gmail.com>
2024-10-23 00:20:59 +0500

    booking room by user and confirmation page, bootstrap of login and registration

--- hotel/urls.py
+++ hotel/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("hotels/", views.list_hotels, name="hotels"),
     path("hotels/<int:hotel_id>/", views.hotel_detail, name="hotel_detail"),
-    path("hotels/<int:hotel_id>/booking", views.booking_room, name="booking_room")
+    path("hotels/<int:hotel_id>/booking", views.booking_room, name="booking_room"),
+    path("hotels/<int:hotel_id>/booking/booked/<int:room_id>", views.booked, name="booked")
 ]
--- hotel/views.py
+++ hotel/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404
 from .models import Hotel, Room
+from users.models import Guest
 from .forms import BookRoomForm
+from django.contrib.auth.decorators import login_required
 
 def list_hotels(request):
     hotels = get_list_or_404(Hotel)
@@ -10,6 +12,7 @@ def hotel_detail(request, hotel_id):
     hotel = get_object_or_404(Hotel, id=hotel_id)
     return render(request, 'hotel_detail.html', {'hotel':hotel})
 
+@login_required
 def booking_room(request, hotel_id):
     hotel = get_object_or_404(Hotel, id=hotel_id)
     rooms = Room.objects.filter(hotel=hotel, reserved=False)
@@ -20,9 +23,17 @@ def booking_room(request, hotel_id):
             room = form.cleaned_data['room']  # Получаем выбранную комнату
             room.reserved = True  # Помечаем как забронированную
             room.save()  # Сохраняем изменения
-            return redirect('hotel_detail', hotel_id=hotel.id)
+
+            guest = Guest.objects.create(user=request.user, room=room)
+
+            return redirect('booked', hotel_id=hotel.id, room_id=room.id)
     else:
         form = BookRoomForm(hotel=hotel)
 
     return render(request, 'booking_room.html', {'hotel': hotel, 'rooms': rooms, 'form': form})
 
+@login_required
+def booked(request, hotel_id, room_id):
+    guest = get_object_or_404(Guest, user = request.user, room_id=room_id)
+    room = get_object_or_404(Room, id = room_id)
+    return render(request, 'booked.html', {'guest': guest, 'room': room})
