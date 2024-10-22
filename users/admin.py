36c7eb662faf495ab542f3bd59f2f60267fbcee8
neemaque <nurmukhan.n.b@gmail.com>
2024-10-23 02:01:23 +0500

    Admin page changes
    access to databases

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Guest, Favorites, Employee, Review
 
 # Register your models here.
+admin.site.register(Guest)
+admin.site.register(Favorites)
+admin.site.register(Employee)
+admin.site.register(Review)
