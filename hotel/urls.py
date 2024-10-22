96f810c19869a29a25ad3caedfaeacb9a1911332
adilalimgozha <alimgozha2003@gmail.com>
2024-10-22 22:07:26 +0500

    pseudo booking added

--- hotel/urls.py
+++ hotel/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("hotels/", views.list_hotels, name="hotels"),
-    path("hotels/<int:hotel_id>/", views.hotel_detail, name="hotel_detail")
+    path("hotels/<int:hotel_id>/", views.hotel_detail, name="hotel_detail"),
+    path("hotels/<int:hotel_id>/booking", views.booking_room, name="booking_room")
 ]
--- hotel/views.py
+++ hotel/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404
 from .models import Hotel, Room
+from .forms import BookRoomForm
 
 def list_hotels(request):
     hotels = get_list_or_404(Hotel)
@@ -8,3 +9,20 @@ def list_hotels(request):
 def hotel_detail(request, hotel_id):
     hotel = get_object_or_404(Hotel, id=hotel_id)
     return render(request, 'hotel_detail.html', {'hotel':hotel})
+
+def booking_room(request, hotel_id):
+    hotel = get_object_or_404(Hotel, id=hotel_id)
+    rooms = Room.objects.filter(hotel=hotel, reserved=False)
+
+    if request.method == 'POST':
+        form = BookRoomForm(request.POST, hotel=hotel)
+        if form.is_valid():
+            room = form.cleaned_data['room']  # Получаем выбранную комнату
+            room.reserved = True  # Помечаем как забронированную
+            room.save()  # Сохраняем изменения
+            return redirect('hotel_detail', hotel_id=hotel.id)
+    else:
+        form = BookRoomForm(hotel=hotel)
+
+    return render(request, 'booking_room.html', {'hotel': hotel, 'rooms': rooms, 'form': form})
+
