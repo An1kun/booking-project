8af0371208fc6dc73440172ceb70b5025d127b26
adilalimgozha <alimgozha2003@gmail.com>
2024-12-11 01:56:04 +0500

    swagger added

--- booking_project/urls.py
+++ booking_project/urls.py
@@ -24,12 +24,28 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.urls import path, re_path
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Booking",
+      default_version='v1',
+      description="Booking API Documentation",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("users.urls")),
     path('', include("hotel.urls")),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 if settings.DEBUG:
